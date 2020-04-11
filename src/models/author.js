3b5eb9cbe09a099a51f156a05256c692356ebc5b
Fabio Kociolek <fabiotkx@gmail.com>
2020-04-11 15:42:05 -0300

    Show author list.

--- src/models/author.js
+++ src/models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 var Schema = mongoose.Schema;
 
 var AuthorSchema = new Schema(
@@ -27,7 +28,19 @@ AuthorSchema
 AuthorSchema
   .virtual('lifespan')
   .get(function () {
-    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+      return (this.date_of_birth && this.date_of_death) ? (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString() : 'unknown';
+});
+
+AuthorSchema
+  .virtual('date_of_birth_formatted')
+  .get(function () {
+      return (this.date_of_birth) ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+});
+
+AuthorSchema
+  .virtual('date_of_death_formatted')
+  .get(function () {
+      return (this.date_of_death) ? moment(this.date_of_death).format('YYYY-MM-DD') : '';
 });
 
 AuthorSchema
